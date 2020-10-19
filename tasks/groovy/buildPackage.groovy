b70abc6b2a6f4010e8762f44f171441fb993fd28
sethlinnpubliq <59605201+sethlinnpubliq@users.noreply.github.com>
2020-10-19 17:29:04 +0200

    Update buildPackage.groovy

--- tasks/groovy/buildPackage.groovy
+++ tasks/groovy/buildPackage.groovy
@@ -3,21 +3,43 @@
 // Groovy installed(latest versio is ok)
 // Node.js version 6.11.1 installed
 // Bundler installed
-// Run: bundler
+// Run: bundle install --deployment --no-color (this installs fpm among other things) 
+import java.text.SimpleDateFormat
+
+String version = ''
+if(this.args[0]){
+   version = this.args[0]
+}
+else{
+   println "No file name provided, using yyyyMMddHHmmss"
+   def now = new Date()
+   version = now.format("yyyyMMddHHmmss") 
+}
 
 //This function runs a bash command, waits for it to finish, and outputs the results.
-def runCommand = { command -> 
-  println command
+def runCommand = { command ->
+  //print command
+  if(command instanceof List) {
+     command.each { print "${it} " }
+     println " "
+  } else {
+     println command
+  }
+    
+  def sout = new StringBuilder(), serr = new StringBuilder()
   def proc = command.execute()
-  proc.in.eachLine { line -> println line }
+  proc.consumeProcessOutput(sout, serr)  
+  //proc.in.eachLine { line -> println line }
   proc.out.close()
   proc.waitFor()
+  println "$sout"
 
   if (proc.exitValue()) {
      println "[ERROR] ${proc.getErrorStream()}"
+      println "${serr}"
+     System.exit(proc.exitValue())
   }
 }
-
 
 println "Creating a debian package from the binaries."
 
@@ -27,7 +49,7 @@ println "Current dir:" + currentDir
 runCommand "composer --version" // For logging purposes.
 runCommand "phing -version"  // For logging purposes.
             
-//runCommand "phing composer" // Run target composer in build.xml
+runCommand "phing composer" // Run target composer in build.xml
 
 //Create new file with content, 
 def configFile = new File("config.yml")
@@ -47,5 +69,16 @@ println configFile.text
 def dir = new File("pkg")
 dir.mkdirs() //Make new directory
 
+// build the fpm command
+List command = ['bundle', 'exec', 'fpm', '-t', 'deb', '-n', 'groepspas-silex', '-v', "${version}", 
+                '-s', 'dir', '-a', 'all', '-p', 'pkg', '-x', '\".git*\"', '-x', 'pkg', '-x', 'config.dist.yml',
+                '--license', '\"Apache-2.0\"', '-m', 'Infra publiq <infra@publiq.be>',
+                '--url', '''"https://www.publiq.be"''', '--vendor', 'publiq vzw',
+                '--description', '\"Silex backend for Groepspas\"', '--prefix', '''/var/www/groepspas-api''',
+                '--deb-user', 'www-data', '--deb-group', 'www-data',
+                '-d', '''php5-cli | php5.6-cli | php7.0-cli | php7.1-cli''',
+                '--config-files', '''/var/www/groepspas-api/config.yml''', '.']
+                
+runCommand command
 
 return this
